ea45f2e600f9cb29c01db641af882281eecaa273
Mohammad Matar <116981460+mo-matar@users.noreply.github.com>
2025-04-13 00:37:09 +0300

    Update aludec.sv

--- RTL/modules/aludec.sv
+++ RTL/modules/aludec.sv
@@ -1,5 +1,5 @@
-`include "ALUControl_Defines.sv"
- module aludec(         input    logic opb5,
+`include "../RTL/ALUControl_Defines.sv"
+module aludec(         input    logic opb5,
                         input    logic [2:0] funct3,
                         input    logic       funct7b5,
                         input    logic [1:0] ALUOp,
