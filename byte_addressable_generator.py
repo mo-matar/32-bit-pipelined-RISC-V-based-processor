075704ef45f0ec5a4cc68d0ad58bbcd2f5354763
mo-matar <116981460+mo-matar@users.noreply.github.com>
2025-04-12 17:47:42 +0300

    Update byte_addressable_generator.py

--- byte_addressable_generator.py
+++ byte_addressable_generator.py
@@ -1,11 +1,9 @@
-# Open the file.dat for reading
-with open("test.txt", "r") as file:
+with open("instructions.txt", "r") as file:
     # Loop through each line in the file
     for line in file:
-        # Remove any trailing whitespace (like newline characters)
+        # Remove any trailing whitespace 
         line = line.strip()
         # Split the line into pairs of two characters
         pairs = [line[i:i+2] for i in range(0, len(line), 2)]
-        # Print each pair
         for pair in pairs:
             print(pair)
